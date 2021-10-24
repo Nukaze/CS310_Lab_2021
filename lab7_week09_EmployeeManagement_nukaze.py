ca06b93a54a3cbf07b468aebaa0ce1c1aa4127be
Kaedehara Nukaze <47706483+acezgtx7@users.noreply.github.com>
2021-10-24 17:49:36 +0700

    done 4 function now refine code

--- lab7_week09_EmployeeManagement_nukaze.py
+++ lab7_week09_EmployeeManagement_nukaze.py
@@ -2,6 +2,8 @@
 #Employee information management Program  #nukaze-
 uidlst, namelst, snamelst, userlst, keylst = [], [], [], [], []
 uid, reqname, fname, name, sname, keyword = "","","","","",""
+
+
 def emp_main():
     print(("> " * 10 + "Welcome Employee System" + " <" * 10).center(64))
     print("[1] Register Employee")
@@ -23,31 +25,36 @@ def emp_main():
         print(("x " *32).center(64))
         emp_main()
 
+
 def emp_regis():
     print("+"*64)
     print("+ + + REGISTER EMPLOYEE + + +".center(64))
     print("+"*64)
     reqname = input("Enter Name Surname: ")
+    fname = reqname.split()
+    if len(fname) > 1:                                    #catch
+        name = fname[0];namelst.append(name)
+        sname = fname[1];snamelst.append(sname)
+        username = name+"."+sname[0]                      #genuser
+        userlst.append(username)
+        ukey = (reqname[1].lower())+(reqname[3].upper())+(reqname[2].upper())+str(len(reqname))+(reqname[4].lower())
+        #genkey
+        keylst.append(ukey)
+        print(namelst)
+        print(snamelst)
+        print(ukey)
+    else:
+        print("Required name and surname!! Please try again.")
+        emp_regis()
     uid = (input("Enter ID card [13 digits]: "))
     while len(uid) != 13:
         print("Invalid ID card ")
         uid = (input("Enter ID card [13 digits]: "))
-        print(len(uid))
     uidlst.append(uid)
-    fname = reqname.split()
-    name = fname[0];namelst.append(name)
-    sname = fname[1];snamelst.append(sname)
-    username = name+"."+sname[0]                      #genuser
-    userlst.append(username)
-    ukey = (reqname[1].lower())+(reqname[3].upper())+(reqname[2].upper())+str(len(reqname))+(reqname[4].lower())
-    #genkey
-    keylst.append(ukey)
-    print(namelst)
-    print(snamelst)
-    print(ukey)
     print(uid)
     print(uidlst)
     print("Register Complete.")
+
     emp_main()
 
 
@@ -55,6 +62,24 @@ def emp_del():
     print("-" * 64)
     print("- - - DELETE EMPLOYEE - - -".center(64))
     print("-" * 64)
+    print(uid)
+    print(namelst)
+    print(snamelst)
+    print(userlst)
+    print(keylst)
+    delname = input("Enter name employee to delete : ")
+    if delname in namelst:
+        delindex = namelst.index(delname)
+        print("Del index = ",delindex)
+        uidlst.pop(delindex)
+        namelst.pop(delindex)
+        snamelst.pop(delindex)
+        userlst.pop(delindex)
+        keylst.pop(delindex)
+        print(delname+"has been deleted.")
+    else:
+        print("Invalid name!! Please try again.")
+        emp_del()
 
     emp_main()
 
@@ -68,8 +93,6 @@ def emp_data():
     if uidlst:
         print("=" * 64)
         for run in range(len(uidlst)):
-            print("=" * 64)
-
             cnt = str(run+1)
             print(cnt.center(4)+str(namelst[run]).ljust(12)+str(snamelst[run]).ljust(12)
                   +str(userlst[run]).center(11)+str(keylst[run]).center(11)+str(uidlst[run]).center(14))
@@ -82,6 +105,5 @@ def emp_data():
         print(" " * 64)
         print("." * 64)
     emp_main()
-
 
 emp_main()
