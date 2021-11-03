2e966faaee201ea9506cadc5847b863c729d04b0
Kaedehara Nukaze <47706483+acezgtx7@users.noreply.github.com>
2021-11-03 23:11:33 +0700

    done function gender and year

--- lab8_week10_universitydb/lab8_week10_universitydb_nukaze.py
+++ lab8_week10_universitydb/lab8_week10_universitydb_nukaze.py
@@ -1,71 +1,122 @@
 # CS310 127G Week09 Lab7 Anupun Khumthong 1640705560
 # university database Program  #nukaze-
 import time
+
 def loadprogress():
     input("> Press any key to analysis raw data to University database ")
     print("> [ Analysing Data.. ]")
     t = 5
     print("> [",end="")
     for i in range(t):
-        time.sleep(0.5)
+        time.sleep(0.25)
         loadprogress = "■"*i*5
         print("%s"%loadprogress,end="")
     print("]")
     time.sleep(1)
     print("> [ 100% Database Analysis Successfully. ]")
 
-def genderCal():
-    std.split()
-    if data[-1].upper() == "F":
-        dbfemale.write(std)
-    elif data[-1].upper() == "M":
-        dbmale.write(std)
+def show_femaletable():
+    with open('femalestdlst.txt','r') as dbfemale:
+        line = dbfemale.read().splitlines()
+        print("\n\nline female = ",line,end="\n")
+        for std in line:
+            print(std)
+        print("Total Female Students = {}\n".format(len(line)))
+
+def show_maletable():
+    with open('malestdlst.txt', 'r') as dbmale:
+        line = dbmale.read().splitlines()
+        print("line male = ",line,end="")
+        for std in line:
+            print(std)
+        print("Total Male Students = {}\n".format(len(line)))
+
+def show_y1table():
+    with open('year1stdlst.txt', 'r') as dby1:
+        line = dby1.read().splitlines()
+        print("line male = ",line,end="")
+        for std in line:
+            print(std)
+        print("Total Year 1st Students = {}\n".format(len(line)))
+
+def show_y2table():
+    with open('year2stdlst.txt', 'r') as dby2:
+        line = dby2.read().splitlines()
+        print("line 2nd = ",line,end="")
+        for std in line:
+            print(std)
+        print("Total Year 2nd Students = {}\n".format(len(line)))
+
+def show_y3table():
+    with open('year3stdlst.txt', 'r') as dby3:
+        line = dby3.read().splitlines()
+        print("line 3rd = ",line,end="")
+        for std in line:
+            print(std)
+        print("Total Year 3rd Students = {}\n".format(len(line)))
+
+def show_y4table():
+    with open('year4stdlst.txt', 'r') as dby4:
+        line = dby4.read().splitlines()
+        print("line 4th = ",line,end="")
+        for std in line:
+            print(std)
+        print("Total Year 4th Students = {}\n".format(len(line)))
+
+def show_yetctable():
+    with open('yearetcstdlst.txt', 'r') as dbyetc:
+        line = dbyetc.read().splitlines()
+        print("line etc = ",line,end="")
+        for std in line:
+            print(std)
+        print("Total Others year Students = {}\n".format(len(line)))
 
 
-def yearCal():
-    cnt = 0
-    data[4]
-
-"""
-dby1 = open('year1stdlst.txt','w+')
-dby2 = open('year2stdlst.txt','w+')
-dby3 = open('year3stdlst.txt','w+')
-dby4 = open('year4stdlst.txt','w+')
-dbyetc = open('yearetcstdlst.txt','w+')
-"""
+loadprogress()
 with open('stdlst.txt','r') as dbstd:
-    dbmale = open('malestdlst.txt', 'w+')
-    dbfemale = open('femalestdlst.txt', 'w+')
-    dbrmale = open('malestdlst.txt', 'r')
-    dbrfemale = open('femalestdlst.txt', 'r')
+    qtyfemale, qtymale = 0, 0
+    qtyy1, qtyy2, qtyy3, qtyy4, qtyyetc = 0, 0, 0, 0, 0
+    dbmale = open('malestdlst.txt', 'w');dbfemale = open('femalestdlst.txt', 'w')
+    dby1 = open('year1stdlst.txt', 'w')
+    dby2 = open('year2stdlst.txt', 'w')
+    dby3 = open('year3stdlst.txt', 'w')
+    dby4 = open('year4stdlst.txt', 'w')
+    dbyetc = open('yearetcstdlst.txt', 'w')
     line = dbstd.read().splitlines()
-    print("line = ",line,end="\n")
-    qtyfemale,qtymale = 0,0
     for std in line:
         data = std.split()
         std.split()
+        #gender
         if data[-1].upper() == "F":
-            dbfemale.write(std+'\n')
+            dbfemale.write(std + '\n')
             qtyfemale += 1
-        elif data[-1].upper() == "M":
-            dbmale.write(std+'\n')
+        if data[-1].upper() == "M":
+            dbmale.write(std + '\n')
             qtymale += 1
+        #year
+        if int(data[4]) == 1:
+            dby1.write(std + '\n')
+            qtyy1 += 1
+        if int(data[4]) == 2:
+            dby2.write(std + '\n')
+            qtyy2 += 1
+        if int(data[4]) == 3:
+            dby3.write(std + '\n')
+            qtyy3 += 1
+        if int(data[4]) == 4:
+            dby4.write(std + '\n')
+            qtyy4 += 1
+        if int(data[4]) > 4 or int(data[4]) < 1:
+            dbyetc.write(std + '\n')
+            qtyyetc += 1
     dbfemale.write("Total female students = {}".format(qtyfemale))
     dbmale.write("Total male students = {}".format(qtymale))
-    dbmale.close()
-    dbfemale.close()
+    dby1.write("Total Student = {}".format(qtyy1))
+    dby2.write("Total Student = {}".format(qtyy2))
+    dby3.write("Total Student = {}".format(qtyy3))
+    dby4.write("Total Student = {}".format(qtyy4))
+    dbyetc.write("Total Student = {}".format(qtyyetc))
 
+    dbmale.close();dbfemale.close()
+    dby1.close();dby2.close();dby3.close();dby4.close();dbyetc.close()
 
-with open('femalestdlst.txt','r') as dbfemale:
-    line = dbfemale.read().splitlines()
-    print("\n\nline female = ",line,end="\n")
-    for std in line:
-        print(std)
-    #print("Total Female Students = {}\n".format(len(line)))
-
-with open('malestdlst.txt', 'r') as dbmale:
-    line = dbmale.read().splitlines()
-    print("line male = ",line,end="")
-    for std in line:
-        print(std)
-    #print("Total Male Students = {}\n".format(len(line)))
